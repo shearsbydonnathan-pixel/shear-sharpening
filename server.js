22a76b4ca8285732231fabd8d35cee2d1b951ba8
shearsbydonnathan-pixel <shearsbydonnathan@gmail.com>
2026-04-13 13:21:45 -0400

    Fix CORS

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: '*' }));());
 
 // ─── Config ───────────────────────────────────────────────────────────────────
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
