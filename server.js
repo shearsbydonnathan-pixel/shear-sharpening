46171d156bbc9eb79204d4e0e517eb213d398fc2
shearsbydonnathan-pixel <shearsbydonnathan@gmail.com>
2026-04-13 19:26:55 -0400

    fix subject.​​​​​​​​​​​​​​​​

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ async function sendBookingEmail(appt) {
     await resend.emails.send({
   from: 'onboarding@resend.dev',
   to: process.env.GMAIL_USER,
-
+subject: `New Booking - ${appt.name}`,
  text: `NEW APPOINTMENT REQUEST\n\nClient: ${appt.name}\nEmail: ${appt.clientEmail}\nPhone: ${appt.phone}\nSalon: ${appt.salon}\nAddress: ${appt.salonAddress}\nDate: ${appt.date}\nTime: ${appt.time}`,
   });
     console.log("📧 Booking email sent.");
